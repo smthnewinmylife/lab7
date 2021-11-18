32270bdc10eb9984c5c58cd1a7da2bd14fe26675
smthnewinmylife <daniil.garaschenko@gmail.com>
2021-11-18 13:45:14 +0200

    2nd

--- ConsoleApplication3.cpp
+++ ConsoleApplication3.cpp
@@ -9,7 +9,6 @@ int main()
     double b, x, z, y;
     cin >> z >> y >> x;
     b = (pow(z, 2 * x) + pow(y, -1) * cos(z + y) * x) / (x + 1) + sin((45 * M_PI) / 180);
-    cout << b;
-    cout << "Thanks" << endl;
+    cout << b;    
     cout << "changes for git";
 }
